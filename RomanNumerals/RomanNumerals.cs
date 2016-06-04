95315a33b15dc840068ff9c246ef49718cb3420f
Jon Graf <jongraf1@gmail.com>
2016-06-04 10:11:27 -0500

    Initial Commit

---
+++ RomanNumerals/RomanNumerals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        internal String Convert(int arabic)
+        {
+            Dictionary<int, string> uniqueCharacters = new Dictionary<int, string>
+            {
+                {1, "I" },
+                {5, "V" },
+                {10, "X" },
+                {50, "L" }
+            };
+
+            foreach (var uniqueCharacter in uniqueCharacters.Keys)
+            {
+                if (arabic == uniqueCharacter)
+                    return uniqueCharacters[uniqueCharacter];
+
+                // when one less...
+
+            }
+
+            if (arabic == 2)
+                return "II";
+
+            if (arabic == 4)
+                return "IV";
+
+            if (arabic == 9)
+                return "IX";
+
+            return "";
+        }
+
+    }
+}
---
+++ RomanNumerals/RomanNumeralsTest.cs
@@ -0,0 +1,22 @@
+﻿namespace RomanNumerals
+{
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class RomanNumeralConverterTest
+    {
+
+        [Test]
+        public void ConvertArabicToRoman()
+        {
+            Assert.AreEqual("", new RomanNumeralConverter().Convert(0));
+            Assert.AreEqual("I", new RomanNumeralConverter().Convert(1));
+            Assert.AreEqual("II", new RomanNumeralConverter().Convert(2));
+            Assert.AreEqual("IV", new RomanNumeralConverter().Convert(4));
+            Assert.AreEqual("V", new RomanNumeralConverter().Convert(5));
+            Assert.AreEqual("IX", new RomanNumeralConverter().Convert(9));
+            Assert.AreEqual("X", new RomanNumeralConverter().Convert(10));
+        }
+    }
+}
